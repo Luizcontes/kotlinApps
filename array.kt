ba0e9e6f38ed94be6f5f55347e77571341198acf
Luiz <luiz.contes@hotmail.com>
2022-03-24 11:21:27 +0000

    Repo created while studying Kotlin

---
+++ array.kt
@@ -0,0 +1,37 @@
+/*
+fun main() {
+
+    val hobbits = arrayOf("Frodo", "Sam", "Merry", "Pippin")
+    var x = 0;
+
+    while (x < 5) {
+        println("${hobbits[x]} is a good Hobbit name")
+        x = x + 1
+    }
+}
+*/
+/* 
+fun main() {
+
+    val firemen = arrayOf("Pugh", "Pugh", "Barney McGrew", "Cuthbert", "Dibble", "Grub")
+    var firemanNo = 0;
+
+    while (firemanNo < 6) {
+        println("Fireman number $firemanNo is ${firemen[firemanNo]}")
+        firemanNo = firemanNo + 1
+    }
+}
+*/
+
+fun main() {
+
+    val fruits = arrayOf("Banana", "BlueBerry", "Pomegranate", "Cherry")
+
+    var x = 0
+
+    while (x < fruits.size) {
+        println("Fruit = ${fruits[x]}")
+        x += 1
+    }
+    
+}
